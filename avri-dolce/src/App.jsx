a4a088fc922ac25fa727951294a37063367be358
Aik-202 <ikogweassurance85@gmail.com>
2024-03-26 03:45:52 +0100

    Added routes for all pages

--- avri-dolce/src/App.jsx
+++ avri-dolce/src/App.jsx
@@ -4,13 +4,24 @@ import {
   Routes, 
   Route} from 'react-router-dom'
 import './App.css'
-import { LandingPage } from './Pages'
+import { AdminLoginPage, AdminPage, CustomersPage, LandingPage, LoginPage, OrdersPage, ProductsPage, SignUpPage, UserPage, ViewProduct } from './Pages'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path = '/' element={<LandingPage/>}></Route>
+        <Route exact path = '/' element={<LandingPage />}></Route>
+        <Route exact path = '/product' element={<ProductsPage />}></Route>
+        <Route exact path = '/view-product' element={<ViewProduct />}></Route>
+
+        <Route exact path = '/user/login' element={<LoginPage />}></Route>
+        <Route exact path = '/user/signup' element={<SignUpPage />}></Route>
+        <Route exact path = '/user' element={<UserPage />}></Route>
+        
+        <Route exact path = '/admin/login' element={<AdminLoginPage />}></Route>
+        <Route exact path = '/admin' element={<AdminPage />}></Route>
+        <Route exact path = '/admin/customers' element={<CustomersPage />}></Route>
+        <Route exact path = '/admin/orders' element={<OrdersPage />}></Route>
       </Routes>
     </Router>
   )
--- avri-dolce/src/Pages/index.jsx
+++ avri-dolce/src/Pages/index.jsx
@@ -5,9 +5,9 @@ export {default as AdminLoginPage} from './AdminPages/AdminLoginPage'
 
 
 export {default as LandingPage} from './LandingPage'
+export {default as ProductsPage} from './ProductsPage'
+export {default as ViewProduct} from './ViewProduct'
 
 export {default as LoginPage} from './UserPages/LoginPage'
 export {default as SignUpPage} from './UserPages/SignUpPage'
 export {default as UserPage} from './UserPages/UserPage'
-export {default as ProductsPage} from './UserPages/ProductsPage'
-export {default as ViewProduct} from './UserPages/ViewProduct'
