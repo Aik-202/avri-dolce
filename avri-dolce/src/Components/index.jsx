f7afd9ff9de4c33834009e92380569d068a63e6c
Aik-202 <ikogweassurance85@gmail.com>
2024-03-26 03:53:54 +0100

    Added more components for the landing, user page and form

--- avri-dolce/src/Components/index.jsx
+++ avri-dolce/src/Components/index.jsx
@@ -2,3 +2,13 @@ export {default as NavBar} from './Navigation/NavBar'
 export {default as NavItems} from './Navigation/NavItems'
 
 export {default as ADLogo} from './ADLogo'
+export {default as Form} from './Form'
+
+export {default as Hero} from './Landing/Hero'
+export {default as Offer} from './Landing/Offer'
+export {default as Info} from './Landing/Info'
+export {default as FAQ} from './Landing/FAQ'
+export {default as Footer} from './Landing/Footer'
+
+export {default as Cart} from './User/Cart'
+export {default as Product} from './User/Product'
