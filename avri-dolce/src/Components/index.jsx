9bccb66380c7e46deaed85e822a282231a43fa2d
Aik-202 <ikogweassurance85@gmail.com>
2024-03-26 03:22:32 +0100

    Exported all files in index.jsx for component and pages, added new file for logo under components, installed react router dom

---
+++ avri-dolce/src/Components/index.jsx
@@ -0,0 +1,4 @@
+export {default as NavBar} from './Navigation/NavBar'
+export {default as NavItems} from './Navigation/NavItems'
+
+export {default as ADLogo} from './ADLogo'
---
+++ avri-dolce/src/Pages/index.jsx
@@ -0,0 +1,13 @@
+export {default as AdminPage} from './AdminPages/AdminPage'
+export {default as CustomersPage} from './AdminPages/CustomersPage'
+export {default as OrdersPage} from './AdminPages/OrdersPage'
+export {default as AdminLoginPage} from './AdminPages/AdminLoginPage'
+
+
+export {default as LandingPage} from './LandingPage'
+
+export {default as LoginPage} from './UserPages/LoginPage'
+export {default as SignUpPage} from './UserPages/SignUpPage'
+export {default as UserPage} from './UserPages/UserPage'
+export {default as ProductsPage} from './UserPages/ProductsPage'
+export {default as ViewProduct} from './UserPages/ViewProduct'
