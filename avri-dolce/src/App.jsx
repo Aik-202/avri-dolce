b71deb95601c8dba03fa186b14f52a7df837602d
Aik-202 <ikogweassurance85@gmail.com>
2024-03-26 02:30:17 +0100

    Added text

--- avri-dolce/src/App.jsx
+++ avri-dolce/src/App.jsx
@@ -1,6 +1,12 @@
+import React from 'react'
 import './App.css'
 
 function App() {
+  return (
+    <div>
+      <p className='text-[#6D0016]'>Hello World</p>
+    </div>
+  )
 }
 
 export default App
