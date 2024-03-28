736f49d3e744362f42888cbe30275d9ec037856d
Aik-202 <ikogweassurance85@gmail.com>
2024-03-28 11:52:02 +0100

    Made some changes

--- avri-dolce/src/Components/Navigation/NavBar.jsx
+++ avri-dolce/src/Components/Navigation/NavBar.jsx
@@ -15,7 +15,7 @@ export default function NavBar() {
         onClick={() => setActive(prev => !prev) }/>
         <NavItems active={active}/>
       </div>
-      <div className='flex flex-row space-x-8 font-b-font font-medium'>
+      <div className='flex flex-row justify-between w-full md:w-auto md:justify-start md:space-x-8 font-b-font font-medium'>
         <ChangeLanguage />
         <button className='rounded-sm border-solid border-[1px] border-dark-green px-3 
         hover:rounded-full'>Sign Up</button>
