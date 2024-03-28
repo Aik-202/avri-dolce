4a81339815f6881236d38deab26a1dba076834a7
Aik-202 <ikogweassurance85@gmail.com>
2024-03-28 10:48:39 +0100

    Made the hero section responsive for desktop

--- avri-dolce/src/Components/Landing/Hero.jsx
+++ avri-dolce/src/Components/Landing/Hero.jsx
@@ -6,11 +6,11 @@ export default function Hero() {
   const { t } = useTranslation();
 
   return (
-    <section className='flex flex-row justify-between w-screen pl-5'>
+    <section className='flex flex-row justify-between w-screen pl-5 h-max'>
       <div className='flex flex-col space-y-5'>
-        <h1 className='font-h-font text-[4.6rem] w-[10rem] tracking-wide leading-[4.2rem] z-10'>
+        <h1 className='font-h-font text-[4.2rem] leading-[3.4rem] xl:text-[4.6rem] w-[10rem] tracking-wide xl:leading-[4.2rem] z-10'>
           <i className='text-red font-bold'>elevate</i> <span>YOUR WARDROBE</span></h1>
-        <p className='font-p-font w-[24rem] text-sm'>Have you been looking foward to a change of wardrobe and dont know what brand to go for? 
+        <p className='font-p-font w-[21rem] xl:w-[24rem] text-sm'>Have you been looking foward to a change of wardrobe and dont know what brand to go for? 
         The search is over!! <i className='text-dark-green font-bold'>Avri Dolce</i> is the way to go, offering you all kinds of clothings 
         at affordable prices!</p>
         <div className='flex flex-row space-x-3 font-b-font font-medium text-sm'>
@@ -21,9 +21,9 @@ export default function Hero() {
           products</button>
       </div>
       </div>
-      <figure className='flex flex-row'>
-        <img src={women} alt="product one" className='w-[28rem] h-[18rem]' />
-        <img src={men} alt="product one" className='w-[32rem]' />
+      <figure className='grid grid-cols-2 grid-rows-1'>
+        <img src={women} alt="product one" className='col-span-1' />
+        <img src={men} alt="product one" className='col-span-1 h-full' />
       </figure>
     </section>
   )
