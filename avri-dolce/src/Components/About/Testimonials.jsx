5a9bb3de4d22e1a47bdabb85264d2c515ef152fa
Aik-202 <ikogweassurance85@gmail.com>
2024-03-29 20:35:59 +0100

    Made the Testimonials component responsive

--- avri-dolce/src/Components/About/Testimonials.jsx
+++ avri-dolce/src/Components/About/Testimonials.jsx
@@ -7,12 +7,13 @@ import { ratings } from '../../Data/demo';
 export default function Testimonials() {
   return (
     <section className='flex flex-col justify-center items-center space-y-10'>
-        <h2 className='font-h-font text-3xl font-bold text-red tracking-wide'>WHAT OUR CUSTOMERS SAY</h2>
+        <h2 className='text-center w-[90%] md:w-auto font-h-font text-3xl font-bold text-red tracking-wide'>
+        WHAT OUR CUSTOMERS SAY</h2>
         <p className='w-[90%] md:w-[80%] font-p-font text-base font-semibold'>It is easy for us to say who we are, 
         but what our customers say about us is most important, the following are the most revelant reviews 
         from our customers coined from Google Reviews. </p>
-        <section className='grid grid-cols-4 gap-8'>
-            {ratings.map((item) => <div className='bg-gray px-5 py-5 rounded-md shadow-md'>
+        <section className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8'>
+            {ratings.map((item) => <div key={item.name} className='bg-gray px-5 py-5 rounded-md shadow-md'>
                 <div className='flex flex-row space-x-2 items-center'>
                     <Avatar sx={{ bgcolor: red[900] }} alt={item.name}>{item.name.charAt(0)}</Avatar>
                     <p className='font-p-font text-base font-semibold'>{item.name}</p>
