c052fec34f155a062abd6789880d85d93e94e43d
Aik-202 <ikogweassurance85@gmail.com>
2024-03-29 20:49:19 +0100

    Added html, styles, images for the contact page

--- avri-dolce/src/Pages/ContactPage.jsx
+++ avri-dolce/src/Pages/ContactPage.jsx
@@ -5,6 +5,12 @@ export default function ContactPage() {
   return (
     <section className='flex flex-col space-y-10'>
       <NavBar contact={true}/>
+      <section className='relative flex justify-center w-screen h-full'>
+        <div className='absolute left-0 w-[50%] h-full bg-dark-green opacity-70'></div>
+        <div className='flex flex-col z-20 bg-gray p-5 rounded-md shadow-md'>
+          <h2 className='font-h-font text-3xl font-bold text-red tracking-wide'>CONTACT US</h2>
+        </div>
+      </section>
     </section>
   )
 }
