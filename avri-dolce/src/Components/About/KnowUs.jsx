d55c25235ae03ebae03b6ede247564d464f464f4
Aik-202 <ikogweassurance85@gmail.com>
2024-03-29 16:29:21 +0100

    Made the know us component responsive for all breakpoints

--- avri-dolce/src/Components/About/KnowUs.jsx
+++ avri-dolce/src/Components/About/KnowUs.jsx
@@ -12,9 +12,9 @@ export default function KnowUs() {
       requirements of its clientele, encompassing the meticulous design of T-shirts, trousers, hoodies, caps, 
       knickers, gym attire, and African-inspired dresses. We are located at 
       <i className='text-red font-bold'> Via Musone, 20, 65129 Pescara PE, Italy</i> </p>
-      <img src={locate} alt="locate" />
-      <div className='grid grid-cols-3 gap-x-10'>
-        {know.map((list) => <div className='flex items-center flex-col w-[20rem]'>
+      <img src={locate} alt="locate" className='self-center w-[90%] md:w-[90%] lg:w-auto' />
+      <div className='grid grid-cols-1 gap-y-10 md:gap-y-0 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10'>
+        {know.map((list) => <div className='flex items-center flex-col w-[17rem] xl:w-[20rem]' key={list.head}>
             <img src={list.img} alt={list.head} className='w-[55px] self-center' />
             <p className='font-p-font text-lg font-bold py-2 text-red'>{list.head}</p>
             <p className='font-p-font text-base font-semibold'>{list.text}</p>
