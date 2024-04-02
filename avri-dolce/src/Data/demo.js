5bf62dfad6a5161f76d2eae4d678ee9cbd713c75
Aik-202 <ikogweassurance85@gmail.com>
2024-04-02 14:29:09 +0100

    Added dummy data for head wear for men

--- avri-dolce/src/Data/demo.js
+++ avri-dolce/src/Data/demo.js
@@ -1,4 +1,4 @@
-import { cap, delivery, fbRed, hours, igRed, locate, locationRed, mb1, mb2, mb3, mb4, mh1, mh2, mh3, mh4, ms1, ms2, ms3, ms4, mss1, mss2, mss3, mss4, mt1, mt2, mt3, mt4, mt5, mt6, p1, p2, p3, p4, p5, p6, p7, p8, tikRed, wardrobe, whatRed } from "."
+import { cap, delivery, fbRed, hours, igRed, locate, locationRed, mb1, mb2, mb3, mb4, mc1, mc2, mc3, mc4, mc5, mh1, mh2, mh3, mh4, ms1, ms2, ms3, ms4, mss1, mss2, mss3, mss4, mt1, mt2, mt3, mt4, mt5, mt6, p1, p2, p3, p4, p5, p6, p7, p8, tikRed, wardrobe, whatRed } from "."
 
 export const homeNav = ['Home', 'About', 'Products', 'Contact']
 export const products = [
@@ -425,7 +425,44 @@ export const men = [
     },
     {
         name: 'Head wear',
-        product: []
+        product: [
+            {
+                id: 1,
+                tag: 'Head wear (Sample 1)',
+                price: '€30',
+                img: mc1
+            },
+            {
+                id: 2,
+                tag: 'Head wear (Sample 2)',
+                price: '€30',
+                img: mc2
+            },
+            {
+                id: 3,
+                tag: 'Head wear (Sample 3)',
+                price: '€30',
+                img: mc3
+            },
+            {
+                id: 4,
+                tag: 'Head wear (Sample 4)',
+                price: '€30',
+                img: mc4
+            },
+            {
+                id: 5,
+                tag: 'Head wear (Sample 5)',
+                price: '€30',
+                img: mc5
+            },
+            {
+                id: 6,
+                tag: 'Head wear (Sample 6)',
+                price: '€30',
+                img: p7
+            },
+        ]
     },
     {
         name: 'African',
