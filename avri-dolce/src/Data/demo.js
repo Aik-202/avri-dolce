d3f6662d0c5bebbe18eb12a05a4652a6178226b2
Aik-202 <ikogweassurance85@gmail.com>
2024-04-02 13:56:32 +0100

    Exported the images for the womens collection of the collection component

--- avri-dolce/src/Data/demo.js
+++ avri-dolce/src/Data/demo.js
@@ -5,49 +5,49 @@ export const products = [
     {
         id: 1,
         tag: 'Custom T-shirt',
-        price: '£30',
+        price: '€30',
         img: p1
     },
     {
         id: 2,
         tag: 'Custom Trousers',
-        price: '£30',
+        price: '€30',
         img: p2
     },
     {
         id: 3,
         tag: 'Custom Sweat-shirt',
-        price: '£30',
+        price: '€30',
         img: p3
     },
     {
         id: 4,
         tag: 'Custom Shorts',
-        price: '£30',
+        price: '€30',
         img: p4
     },
     {
         id: 5,
         tag: 'Custom Hoodies',
-        price: '£30',
+        price: '€30',
         img: p5
     },
     {
         id: 6,
         tag: 'African',
-        price: '£30',
+        price: '€30',
         img: p6
     },
     {
         id: 7,
         tag: 'Custom Cap',
-        price: '£30',
+        price: '€30',
         img: cap
     },
     {
         id: 8,
         tag: 'Full Gym Clothes',
-        price: '£30',
+        price: '€30',
         img: p8
     },
 ]
@@ -55,49 +55,49 @@ export const collection = [
     {
         id: 1,
         tag: 'T-shirts',
-        price: '£30',
+        price: '€30',
         img: p1
     },
     {
         id: 2,
         tag: 'Bottoms',
-        price: '£30',
+        price: '€30',
         img: p2
     },
     {
         id: 3,
         tag: 'Sweat-shirts',
-        price: '£30',
+        price: '€30',
         img: p3
     },
     {
         id: 4,
         tag: 'Head wear',
-        price: '£30',
+        price: '€30',
         img: p7
     },
     {
         id: 5,
         tag: 'Hoodies',
-        price: '£30',
+        price: '€30',
         img: p5
     },
     {
         id: 6,
         tag: 'African',
-        price: '£30',
+        price: '€30',
         img: p6
     },
     {
         id: 7,
         tag: 'Gym Clothes',
-        price: '£30',
+        price: '€30',
         img: p8
     },
     {
         id: 8,
         tag: 'Shorts',
-        price: '£30',
+        price: '€30',
         img: p4
     },
 ]
