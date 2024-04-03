d3ad440bf3d9d4e1dad3f3bc718cc78317202d46
Aik-202 <ikogweassurance85@gmail.com>
2024-04-03 05:38:20 +0100

    Added bottom padding to the view product page

--- avri-dolce/src/Pages/ViewProduct.jsx
+++ avri-dolce/src/Pages/ViewProduct.jsx
@@ -16,7 +16,8 @@ export default function ViewProduct() {
       <p className='w-[90%] md:w-[80%] text-center text-base font-bold text-[#F44336] tracking-wide pl-5 pb-5'>! You cannot make in-app 
       purchase, feature hasn't been provided, all transactions will be redirected to 
       whatsapp for now.</p>
-      <section className='px-5 flex flex-col space-y-10 md:space-y-0 md:flex-row justify-between w-[inherit]'>
+      <section className='px-5 flex flex-col space-y-10 md:space-y-0 md:flex-row 
+      justify-between w-[inherit] pb-5'>
         <figure className='md:w-[40%] xl:w-[30%]'>
           <img src={data.img} alt={data.tag} className='cursor-pointer bg-brown rounded-md 
           h-[20rem] lg:h-[26rem] p-5 w-full'  />
