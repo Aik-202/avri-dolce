b821a82cd30efb1b98c7ea1227a88668e8b3ae34
Aik-202 <ikogweassurance85@gmail.com>
2024-04-03 04:07:37 +0100

    Made the view product page reponsive for all break points

--- avri-dolce/src/Pages/ViewProduct.jsx
+++ avri-dolce/src/Pages/ViewProduct.jsx
@@ -10,14 +10,14 @@ export default function ViewProduct() {
   return (
     <section className='flex flex-col space-y-10 w-screen'>
       <NavBar product={true} />
-      <section className='px-5 flex flex-row justify-between w-[inherit]'>
-        <figure className='w-[30%]'>
+      <section className='px-5 flex flex-col space-y-10 md:space-y-0 md:flex-row justify-between w-[inherit]'>
+        <figure className='md:w-[40%] xl:w-[30%]'>
           <img src={data.img} alt={data.tag} className='cursor-pointer bg-brown rounded-md 
-          h-[26rem] p-5 w-full'  />
+          h-[20rem] lg:h-[26rem] p-5 w-full'  />
         </figure>
-        <div className='w-[50%] flex flex-col space-y-10'>
+        <div className='md:w-[50%] flex flex-col space-y-10'>
           <p className='uppercase text-base font-bold text-red tracking-wide'>Avri Dolce</p>
-          <h2 className='uppercase text-4xl font-bold text-black tracking-wide'>{data.tag}</h2>
+          <h2 className='uppercase text-center md:text-start text-4xl font-bold text-black tracking-wide'>{data.tag}</h2>
           <p className='w-[90%] md:w-[80%] font-p-font text-sm font-normal text-[#795548]'>{data.des}</p>
           <p className='uppercase text-3xl font-bold text-black tracking-wide'>{data.price}</p>
         </div>
