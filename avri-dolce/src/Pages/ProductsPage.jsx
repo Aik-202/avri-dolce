8c06be20a058e9c61d07ae40484a73b5c55ae5d6
Aik-202 <ikogweassurance85@gmail.com>
2024-04-13 12:44:49 +0100

    Removed added text

--- avri-dolce/src/Pages/ProductsPage.jsx
+++ avri-dolce/src/Pages/ProductsPage.jsx
@@ -15,9 +15,6 @@ export default function ProductsPage() {
   return (
     <section className='flex flex-col space-y-10'>
       <NavBar product={true}/>
-      <p className='w-[90%] md:w-[80%] text-center text-base font-bold text-[#F44336] tracking-wide pl-5 pb-5'>! You cannot make in-app 
-      purchase, feature hasn't been provided, all transactions will be redirected to 
-      whatsapp for now.</p>
       <Collection option={option} setOption={setOption} type={"MEN'S COLLECTION"}/>
       <Collection option={option} setOption={setOption} type={"WOMEN'S COLLECTION"}/>
       <Footer />
