84987efe094256238d843a57065d62c1c70b4065
Aik-202 <ikogweassurance85@gmail.com>
2024-04-13 08:06:54 +0100

    Added some text

--- avri-dolce/src/Pages/AboutPage.jsx
+++ avri-dolce/src/Pages/AboutPage.jsx
@@ -5,6 +5,9 @@ export default function AboutPage() {
   return (
     <section className='flex flex-col space-y-10'>
       <NavBar about={true}/>
+      <p className='w-[90%] md:w-[80%] text-center text-base font-bold text-[#F44336] tracking-wide pl-5 pb-5'>! You cannot make in-app 
+      purchase, feature hasn't been provided, all transactions will be redirected to 
+      whatsapp for now.</p>
       <About />
       <KnowUs />
       <Ceo />
--- avri-dolce/src/Pages/LandingPage.jsx
+++ avri-dolce/src/Pages/LandingPage.jsx
@@ -5,6 +5,9 @@ export default function LandingPage() {
   return (
     <section className='flex flex-col space-y-10'>
       <NavBar home={true}/>
+      <p className='w-[90%] md:w-[80%] text-center text-base font-bold text-[#F44336] tracking-wide pl-5 pb-5'>! You cannot make in-app 
+      purchase, feature hasn't been provided, all transactions will be redirected to 
+      whatsapp for now.</p>
       <Hero />
       <Ceo />
       <Offer />
