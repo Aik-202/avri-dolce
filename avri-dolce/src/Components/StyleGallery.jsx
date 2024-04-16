874a0c547908fadd785f1f6cd86604a1f41d6657
Aik-202 <ikogweassurance85@gmail.com>
2024-04-16 17:30:16 +0100

    Added the second part of the new component

--- avri-dolce/src/Components/StyleGallery.jsx
+++ avri-dolce/src/Components/StyleGallery.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { gallery } from '../Data/demo'
 
 export default function StyleGallery() {
   return (
     <section className='flex flex-col justify-center items-center space-y-10' id='offer'>
       <h2 className='font-h-font text-3xl font-bold text-red tracking-wide'>AVRI DOLCE'S GALLERY</h2>
-      <p className='w-[90%] md:w-[80%] font-p-font text-base font-semibold'>Check out our gallery containing images of our customers wearing our products. </p>
+      <p className='w-[90%] md:w-[80%] font-p-font text-base font-semibold'>Step into the 
+      Avri Dolce gallery, where elegance meets community. Explore our CEO and customers adorned 
+      in Avri Dolce's finest, embodying style and unity in every stitch. </p>
+      <section className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 w-[90%] 
+      md:w-[80%'>
+        {gallery.map((src) => <figure className={`bg-red bg-opacity-10 max-w-max rounded-md 
+        h-max ${(gallery.indexOf(src) + 1) % 2 === 0 ? 'mt-10' : 'mt-0'}`}>
+            <img src={src} alt="image" className={`max-w-[70%] mx-auto py-5`} />
+            <p className='font-h-font text-center text-red font-medium'>
+            {gallery.indexOf(src) + 1}</p>
+        </figure>)}
+      </section>
     </section>
   )
 }
