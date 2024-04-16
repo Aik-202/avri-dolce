1fc2c62d898c37764cdce912b0ddda4185656541
Aik-202 <ikogweassurance85@gmail.com>
2024-04-16 17:31:19 +0100

    Made the component responsive

--- avri-dolce/src/Components/StyleGallery.jsx
+++ avri-dolce/src/Components/StyleGallery.jsx
@@ -4,7 +4,8 @@ import { gallery } from '../Data/demo'
 export default function StyleGallery() {
   return (
     <section className='flex flex-col justify-center items-center space-y-10' id='offer'>
-      <h2 className='font-h-font text-3xl font-bold text-red tracking-wide'>AVRI DOLCE'S GALLERY</h2>
+      <h2 className='font-h-font text-3xl font-bold text-red tracking-wide text-center 
+      md:text-start'>AVRI DOLCE'S GALLERY</h2>
       <p className='w-[90%] md:w-[80%] font-p-font text-base font-semibold'>Step into the 
       Avri Dolce gallery, where elegance meets community. Explore our CEO and customers adorned 
       in Avri Dolce's finest, embodying style and unity in every stitch. </p>
