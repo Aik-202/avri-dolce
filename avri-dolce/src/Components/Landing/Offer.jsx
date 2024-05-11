9742b35604228292f8b9ec2204214d29fb41cf1e
Aik-202 <ikogweassurance85@gmail.com>
2024-05-11 16:48:48 +0100

    Modified auth option for offer component

--- avri-dolce/src/Components/Landing/Offer.jsx
+++ avri-dolce/src/Components/Landing/Offer.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import SlideShow from './SlideShow'
 import { products } from '../../Data/demo.js'
 import { g18, left, right } from '../../Data/index.jsx'
+import AuthOptions from '../Navigation/AuthOptions.jsx';
 
 export default function Offer() {
   const [view, setView] = React.useState(0);
   const [count, setCount] = React.useState(0);
   const [width, setWidth] = React.useState(screen.width)
   const [num, setNum] = React.useState(1);
+  const [option, setOption] = React.useState(true)
 
   React.useEffect(() => {
     window.addEventListener('resize', () => {
@@ -22,7 +24,7 @@ export default function Offer() {
   }, [width])
 
   return (
-    <section style={{backgroundImage: `url(${g18})`}} className='relative w-screen h-[41rem]
+    <section style={{backgroundImage: `url(${g18})`}} className='relative w-screen h-[45rem]
     bg-cover' id='offer'>
       <div className='absolute top-0 w-full h-full bg-white opacity-75 z-0'></div>
       <div className='absolute top-0 w-full h-full flex flex-col justify-center items-center 
@@ -33,10 +35,13 @@ export default function Offer() {
         caps, knickers, gym attire, and African-inspired dresses.Some of our recent works are shown below, 
         click on any to see more about the or click on the cart to add to your cart.  </p>
         <div className='absolute top-0 w-[95%] h-full flex flex-row justify-between items-center 
-      space-y-10  cursor-pointer'>
-        <img src={left} alt="left" className='w-[50px]' onClick={ () => {setView(view => view == 0 ? view : view - num); setCount(count => count +-1)}}/>
-        <img src={right} alt="right" className='w-[50px]' onClick={ () => {setView(view => view + num); setCount(count => count + 1)}}/>
-      </div>
+        space-y-10 cursor-pointer'>
+          <img src={left} alt="left" className='w-[50px]' onClick={ () => {setView(view => view == 0 ? view : view - num); setCount(count => count +-1)}}/>
+          <img src={right} alt="right" className='w-[50px]' onClick={ () => {setView(view => view + num); setCount(count => count + 1)}}/>
+        </div>
+        <div className='sticky z-20'>
+          <AuthOptions options={option} setOption={setOption} offer={true} />
+        </div>
         <SlideShow data={products} gender={'Men'} num={num} count={count} setCount={setCount} view={view} setView={setView} offer={true} />
       </div>
     </section>
--- avri-dolce/src/Components/Navigation/AuthOptions.jsx
+++ avri-dolce/src/Components/Navigation/AuthOptions.jsx
@@ -6,19 +6,27 @@ export default function AuthOptions(props) {
     <nav className='flex flex-row space-x-10 font-p-font font-semibold tracking-wider'>  
         <a className={`border-solid border-transparent border-[2px] cursor-pointer 
          hover:border-b-red flex flex-row space-x-2 items-center text-lg
-        ${props.options ? 'text-black' 
+        ${props.options && !props.offer ? 'text-black' 
+        : props.options && props.offer ? 'text-red border-b-red' 
+        :!props.options && props.offer ? 'text-black' 
         : 'text-brown'}`} 
         onClick={() => props.setOption(prev => !prev)}>
-            <img src={props.options ? imgActive : img} alt="image-icon" className='w-[25px]' />
-            <span>Images</span>
+            {!props.offer ? <>
+              <img src={props.options ? imgActive : img} alt="image-icon" className='w-[25px]' />
+              <span>Images</span>
+            </> : <span className='font-bold'>For Men</span> }
         </a>
         <a className={`border-solid border-transparent border-[2px] cursor-pointer 
         text-lg hover:border-b-red flex flex-row space-x-2 items-center
-        ${props.options ? 'text-brown' 
+        ${props.options && !props.offer  ? 'text-brown' 
+        : !props.options && props.offer ? 'text-red border-b-red'
+        : props.options && props.offer ? 'text-black'
         : 'text-black'}`} 
         onClick={() => props.setOption(prev => !prev)}>
-            <img src={props.options ? vid : vidActive} alt="video-icon" className='w-[25px]' />
-            <span>Videos</span>
+            {!props.offer ? <>
+              <img src={props.options ? vid : vidActive} alt="video-icon" className='w-[25px]' />
+              <span>Videos</span>
+            </>: 'For Women'}
         </a>
     </nav>
   )
