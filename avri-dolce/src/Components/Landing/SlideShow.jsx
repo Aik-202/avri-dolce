4c40b54a214d09e525c6cfaead6a28c3de4cb531
Aik-202 <ikogweassurance85@gmail.com>
2024-05-16 14:10:30 +0100

    Made changes

--- avri-dolce/src/Components/Landing/SlideShow.jsx
+++ avri-dolce/src/Components/Landing/SlideShow.jsx
@@ -5,7 +5,8 @@ import { useNavigate } from 'react-router-dom'
 export default function SlideShow(props) {
   const [two, setTwo] = React.useState()
   const [tip, setTip] = React.useState(false)
-
+  const [like, setLike] = React.useState(false)
+ 
   const navigate = useNavigate()
 
   const numGroups = props.offer ? Math.ceil(props.data.length / props.num) : null;
@@ -23,7 +24,7 @@ export default function SlideShow(props) {
   return (
     <section className={`grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-[90%] md:w-auto
     ${props.offer ? 'xl:w-[80%]' : 'xl:w-[100%]'}`}>
-        {props.data && props.offer ? group.map((src => <div className={`flex flex-col cursor-pointer sticky z-10`} key={src.id}>
+        {props.data && props.offer ? group.map((src => <div className={`flex flex-col cursor-pointer`} key={src.id}>
           <figure className={`self-center md:self-start flex flex-col rounded-md px-5 py-2 mb-3
           bg-brown w-[14rem] h-[16rem] items-center
           ${src.id % 2 === 0 ? 'mt-10' : 'mt-0'}`}>
@@ -57,9 +58,9 @@ export default function SlideShow(props) {
           <figure className={`self-center md:self-start flex flex-col rounded-md px-5 py-2 mb-3
           bg-brown w-[14rem] h-[16rem] items-center
           ${src.id % 2 === 0 ? 'mt-10' : 'mt-0'}`}>
-              <img src={ heartBlack} alt="heart" className='w-[35px] h-[35px] self-end 
-              cursor-pointer bg-white rounded-full p-2' id={src.tag} onClick={(e) => { e.target.src.includes('heart-black.png') ? e.target.src = heartRed 
-              : e.target.src = heartBlack } } />
+              <img src={ like ? heartRed : heartBlack} alt="heart" className='w-[35px] h-[35px] self-end 
+              cursor-pointer bg-white rounded-full p-2' id={src.tag} 
+              onClick={() => setLike(prev => !prev) } />
               <div className='flex items-center h-[90%]'>
                 <img key={src.tag} src={src.img} alt={src.tag} className='self-center h-[inherit]'
                  onClick={() => navigate(`/view-product/${src.tag}`, 
